e65686376ae32fd90e26eda64aad8723ac7d407a
eradicador <janisepinto@yahoo.com>
2020-06-19 17:16:27 -0400

    added code to html and scrpit files

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 //display current day using moment.js at the top of the calendar
 $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
+// localStorage.setItem('key', 'value')
+$(".saveBtn").click(function () {
+  var textValue = $(".text-"+$(this).attr("data-block")).val();
+  var textKey = $(this).attr("data-block");
+  console.log(textValue);
+  localStorage.setItem(textKey,textValue)
+});
+console.log(localStorage.getItem("9"));
+console.log(localStorage.getItem("10"));
+
+
+// get item from localStorage put it onto the page
+// compare Times 
+// datablock has the times moment().hour()
+// if statement color for the hour
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
@@ -14,24 +29,25 @@ $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 // WHEN I refresh the page
 // THEN the saved events persist
 
+// var saveBtn = document.querySelector("description");
+// saveBtn.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   init();
+//   storeDay();
 
-init();
-storeDay();
+// function init() {
+//   // Get stored todos from localStorage
+//   // Parsing the JSON string to an object
+//   var storedDay = JSON.parse(localStorage.getItem("textarea"));
 
-function init() {
-  // Get stored todos from localStorage
-  // Parsing the JSON string to an object
-  var storedDay = JSON.parse(localStorage.getItem("textarea"));
+//   // If todos were retrieved from localStorage, update the todos array to it
+//   if (storedDay !== null) {
+//     textarea = storedDay;
+//   }
 
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedDay !== null) {
-    textarea = storedDay;
-  }
-
-
-}
-var textarea = [];
-function storedDay() {
-  // Stringify and set "todos" key in localStorage to todos array
-  localStorage.setItem("textarea", JSON.stringify(textarea));
-}
+// }
+// var textarea = [];
+// function storedDay() {
+//   // Stringify and set "todos" key in localStorage to todos array
+  
+// }
