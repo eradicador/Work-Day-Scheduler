f39aeaa6a6f18d5d606397f2af3fb2da2491fac2
eradicador <janisepinto@yahoo.com>
2020-06-19 00:13:11 -0400

    working on code for the local storage

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 // THEN the saved events persist
 
 
-// init();
-// event();
-// function init() {
-//     // Get stored todos from localStorage
-//     // Parsing the JSON string to an object
-//     var storedTodos = JSON.parse(localStorage.getItem("todos"));
-  
-//     // If todos were retrieved from localStorage, update the todos array to it
-//     if (storedTodos !== null) {
-//       todos = storedTodos;
-//     }
-  
-//     // Render todos to the DOM
-//     renderTodos();
-//   }
-  
-  function event() {
-    // Stringify and set "todos" key in localStorage to todos array
-    localStorage.setItem("todos", JSON.stringify(todos));
+init();
+storeDay();
+
+function init() {
+  // Get stored todos from localStorage
+  // Parsing the JSON string to an object
+  var storedDay = JSON.parse(localStorage.getItem("textarea"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedDay !== null) {
+    textarea = storedDay;
   }
+
+
+}
+var textarea = [];
+function storedDay() {
+  // Stringify and set "todos" key in localStorage to todos array
+  localStorage.setItem("textarea", JSON.stringify(textarea));
+}
