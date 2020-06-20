0af7935a0b8fcaa0bcfccb255a0fffa044cf0754
eradicador <janisepinto@yahoo.com>
2020-06-19 22:59:42 -0400

    added final comments to js file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,18 @@
-// alert("hola");
 //display current day using moment.js at the top of the calendar
 $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
-// localStorage.setItem('key', 'value') saved key and value to local storage
+//when i clicked the saveBtn calls this function()
 $(".saveBtn").click(function () {
+  //to get the value and the key the function selects the attribute in .text-, .val() returns the value attribute
   var textValue = $(".text-" + $(this).attr("data-block")).val();
+  //to get the key $(this) selects the attribute from data-block
   var textKey = $(this).attr("data-block");
-  console.log(textValue);
+  // saved ('key' and 'value')to local storage
   localStorage.setItem(textKey, textValue)
 });
-console.log(localStorage.getItem("9"));
-console.log(localStorage.getItem("10"));
-console.log(localStorage.getItem("11"));
-console.log(localStorage.getItem("12"));
-console.log(localStorage.getItem("1p"));
-console.log(localStorage.getItem("2p"));
-console.log(localStorage.getItem("3p"));
-console.log(localStorage.getItem("4p"));
-console.log(localStorage.getItem("5p"));
-// get item from localStorage put it on the page even if i refresh the browser
+
+//get the value from localStorage put it on the page even if i refresh the browser
+//with jquery selector: select class .text-9 to 5p, .val returns the value attribute from key in the local Storage
  $('.text-9').val(localStorage.getItem('9'))
  $('.text-10').val(localStorage.getItem('10'))
  $('.text-11').val(localStorage.getItem('11'))
@@ -28,10 +22,12 @@ console.log(localStorage.getItem("5p"));
  $('.text-3p').val(localStorage.getItem('3p'))
  $('.text-4p').val(localStorage.getItem('4p'))
  $('.text-5p').val(localStorage.getItem('5p'))
-//  Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+ //moment.js gets hour
 moment().hour()
-
+//  Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+//this function gets the color for past, present and future depending on the hour   
 function hourColor(){
+  //variable to get hours from moment.js 
   var currentH = moment().hours();
   console.log("Current Hour: "+currentH);
   $(".row").each(function(){
@@ -54,15 +50,11 @@ function hourColor(){
 
   })
 }
-
 hourColor();
-
-
 
 // compare Times 
 // datablock has the times moment().hour()
 // if statement color for the hour
-
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
